dd8e629e708ce6cd6c7c7f21179bc25dda759c52
IvanM1987 <jesmon1987@gmail.com>
2018-09-19 19:42:55 +0300

    copy

---
+++ лекция 5.py
@@ -0,0 +1,42 @@
+import my_game_lib as mgl
+
+
+print(__name__)
+# mgl.pow(2, 2)
+#
+# print(my_game_lib.pow(2, 2))
+# print(pow(2, 2))
+
+# from my_game_lib import pow as my_pow
+# from my_game_lib import *
+#
+# generate_person()
+# attack()
+
+# print(pow(2, 2))
+# import lib.test
+#
+# import sys
+#
+# for path in sys.path:
+#     print(path)
+
+# Ознакомиться
+## pip, requirements.txt
+#
+# # https://docs.python.org/3/library/os.html
+#
+# import os
+#
+# # print(os.getcwd())
+# # print(os.chdir())
+#
+#
+#
+# import sys
+#
+# print(sys.argv)
+#
+# if 'help' in sys.argv:
+#     print('Вы запросили помощь!')
+
